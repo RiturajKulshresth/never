50cc6e2ff80ed1f0a9c801aa845e27aec24142d3
Rituraj Kulshresth <netrunner@pop-os.localdomain>
2020-12-23 22:24:24 +0530

    shaayad

---
+++ src/components/header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Header extends React.Component{
+
+
+    
+    render(){
+    return (
+        <header >
+            <div className="logo">Logo</div>
+            <input></input>
+        </header>
+    )}
+}
+
+
+// let styles={
+//     header:{
+//         background:"#03a9fa"
+//     },
+//     logo:{
+//         color:"#fff",
+//         fontFamily:"Roboto",
+//         textAlign:"center"
+//     }
+// }
+export default Header;
