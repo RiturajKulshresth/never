23a4aa37198132755ec49ddba69b646d4cf5559c
Rituraj Kulshresth <netrunner@pop-os.localdomain>
2020-12-24 13:34:52 +0530

    shaayad

--- src/components/header.js
+++ src/components/header.js
@@ -2,13 +2,39 @@ import React from 'react';
 
 class Header extends React.Component{
 
+    state={
+        name:'Farnces',
 
+        keywords:''
+    }
+    inphand=(event, name)=>{
+        this.setState({
+            keywords:event.target.value
+        })
+        console.log(event.target.value)
+        this.hello()
+    }
+    hello(){
+        console.log('hello')
+    }
     
     render(){
     return (
-        <header >
+        <header
+            onClick={()=>console.log('i waas clicked')}
+        >
             <div className="logo">Logo</div>
-            <input></input>
+            <input
+               onChange={(e)=> this.inphand(e,'rin')}
+
+            
+            ></input>
+            <div>
+                {this.state.name}
+            </div>
+            <div>
+                {this.state.keywords}
+            </div>
         </header>
     )}
 }
